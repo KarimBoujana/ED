376bcac9f5f75c608f3c69b6a2ca28c85ff52820
fausto <pepew@newio.com>
2025-05-12 12:26:31 +0200

    MUBIES FLIX & CHILL

---
+++ practica3/exercici1i2/MubiesflixBST.h
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include "BSTtree.h"
+#include "Peli.h"
+using namespace std;
+
+class MubiesflixBST : protected BSTtree<int, Peli> {
+
+    public:
+        enum AdditionStrategy { AFTER_LARGEST_ID, SMALLEST_NOTTAKEN_ID };
+        MubiesflixBST(AdditionStrategy addition_strategy);
+        MubiesflixBST (AdditionStrategy addition_strategy, string file_path);
+        MubiesflixBST (const MubiesflixBST & orig);
+        virtual ~MubiesflixBST ();
+        void loadFromFile(string file_path);
+        void showAllPelis() const;
+        void showPelisByDirector(int director_id) const;
+        float getAverageValoracioOfDirector(int director_id) const;
+        int findLargestDirectorId() const;
+        int findSmallestNotTakenDirectorId() const;
+        void addPeli();
+
+    private:
+        AdditionStrategy addition_strategy;
+        /* Metodes auxiliars, definiu-los aquí sota */
+        string file_path;
+};
+
+MubiesflixBST::MubiesflixBST(AdditionStrategy addition_strategy) {
+    this->addition_strategy = addition_strategy;
+};
+
+MubiesflixBST::MubiesflixBST(AdditionStrategy addition_strategy, string file_path) {
+    this->addition_strategy = addition_strategy;
+    this->file_path = file_path;
+};
+
+MubiesflixBST::MubiesflixBST (const MubiesflixBST & orig) {
+
+    //TODO: hacer copia de la estructura
+
+}
+
+MubiesflixBST::~MubiesflixBST () {
+    //TODO: destruir
+}
+
+void MubiesflixBST::loadFromFile(string file_path) {
+
+}
+
+void MubiesflixBST::showAllPelis() const {
+
+}
+
+void MubiesflixBST::showPelisByDirector(int director_id) const {
+
+}
+
+float MubiesflixBST::getAverageValoracioOfDirector(int director_id) const {
+
+}
+
+int MubiesflixBST::findLargestDirectorId() const {
+
+}
+
+int MubiesflixBST::findSmallestNotTakenDirectorId() const {
+
+}
+
+void MubiesflixBST::addPeli() {
+
+}
